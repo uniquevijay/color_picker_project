4c1b2170b6e0325972b822a7221faee347843864
Vijay <vijaybabuk345@gmail.com>
2024-08-03 13:38:37 +0530

    color commit

---
+++ colorpicker.js
@@ -0,0 +1,26 @@
+ let bgColor = document.getElementById("colorPickerContainer");
+ let spanElement = document.getElementById("selectedColorHexCode");
+
+ function button1Event() {
+    bgColor.style.backgroundColor = "#e0e0e0";
+    spanElement.textContent = "#e0e0e0"
+    spanElement.style.color = "#49a6e9"
+ }
+
+ function button2Event() {
+    bgColor.style.backgroundColor = "#6fcf97";
+    spanElement.textContent = "#6fcf97"
+    spanElement.style.color = "#49a6e9"
+ }
+
+ function button3Event() {
+    bgColor.style.backgroundColor = "#56ccf2";
+    spanElement.textContent = "#56ccf2"
+    spanElement.style.color = "#49a6e9"
+ }
+
+ function button4Event() {
+    bgColor.style.backgroundColor = "#bb6bd9";
+    spanElement.textContent = "#bb6bd9"
+    spanElement.style.color = "#49a6e9"
+ }
